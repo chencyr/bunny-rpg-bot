676186a1e8184a5c66bc70b805bdd6891092749f
rexchen <chen.cyr@gmail.com>
2021-06-11 23:24:09 +0800

    move router modules

--- routers/index.js
+++ routers/index.js
@@ -9,8 +9,14 @@
  */
 module.exports = function(app) {
 
+    const IndexController = app.controller('index-controller');
     const AuthController = app.controller('auth');
+    const LineWebhookController = app.controller('line-webhook');
 
+    const lineConfig = app.config('line');
+    const line = require('@line/bot-sdk');
+
+    app.get('/', IndexController.index);
     app.get('/register', AuthController.register);
-
+    app.post('/webhook/line', line.middleware(lineConfig), LineWebhookController.received);
 };
