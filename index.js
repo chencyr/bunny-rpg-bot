bf97c694d1b50d37ba971630eb82eacd8a246735
rexchen <chen.cyr@gmail.com>
2021-06-11 23:46:58 +0800

    add force https

--- index.js
+++ index.js
@@ -92,6 +92,14 @@ app.controller = function(module) {
     return new Proxy(instance, handler);
 };
 
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
+app.use(requireHTTPS);
 
 const router = require(global.path.routers + '/index.js');
 router(app);
