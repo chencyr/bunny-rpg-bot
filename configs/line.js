6aaf07e31785589c5fab4d2f7724be69f488ffca
rexchen <chen.cyr@gmail.com>
2021-06-11 22:11:00 +0800

    build config

---
+++ configs/line.js
@@ -0,0 +1,11 @@
+/**
+ * Configuration for LINE applications.
+ *
+ * @type {{clientID: string, channelAccessToken: string, channelSecret: string}}
+ */
+module.exports = {
+    channelAccessToken: 'LYgx9c/11du19BXx6rQdj1Wv9FXDY2mtYrEyRT6+43K82n+dMQZ9jpHd46V0dQbRrisS/oVKWZ/fUjZtl5LXbliiRqWpV7y2qEFFfgIhaZN9DQ0TTVo0gMCpW/ZTxbUF9tdLh/k2AkffG4Pyj2RSvgdB04t89/1O/w1cDnyilFU=',
+    channelSecret: '0ae36251c5000662f528b78c0fd318c0',
+    clientID: '1656090906',
+
+};
