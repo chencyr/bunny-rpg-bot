b4601ad6df83f6135fcbdf34c9a398085ad213c8
rexchen <chen.cyr@gmail.com>
2021-06-11 18:25:18 +0800

    add register page by ejs

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
 // CommonJS
 const express = require('express');
-// var fs = require('fs');
-// var sys = require('sys');
 const line = require('@line/bot-sdk');
 
 
 const config = {
     channelAccessToken: 'LYgx9c/11du19BXx6rQdj1Wv9FXDY2mtYrEyRT6+43K82n+dMQZ9jpHd46V0dQbRrisS/oVKWZ/fUjZtl5LXbliiRqWpV7y2qEFFfgIhaZN9DQ0TTVo0gMCpW/ZTxbUF9tdLh/k2AkffG4Pyj2RSvgdB04t89/1O/w1cDnyilFU=',
-    channelSecret: '0ae36251c5000662f528b78c0fd318c0'
+    channelSecret: '0ae36251c5000662f528b78c0fd318c0',
+    clientID: '1656090906',
 };
-
-// new line.Client(config);
-// line.middleware(config);
 
 
 const app = express();
 app.set("view options", {layout: false});
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+
 app.use('/static', express.static(__dirname + '/static'));
 
 
 app.get('/', function(req, res){
     res.send('Service Available!');
+});
+
+
+app.get('/register', function(req, res){
+    res.render('register', {
+        client_id: config.clientID,
+    });
 });
 
 app.post('/webhook', line.middleware(config), (req, res) => {
