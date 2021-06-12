f41a63795dd8d78ce74f2e2de84d17a464ba6855
rexchen <chen.cyr@gmail.com>
2021-06-12 14:45:32 +0800

    implement architecture

---
+++ core/events/event-factory/line-factory.js
@@ -0,0 +1,17 @@
+const Factory = require('./event-factory');
+
+/**
+ * Line event factory
+ */
+class LineEventFactory extends Factory {
+
+    /**
+     * Create command event instance by adapter.
+     * @param event
+     */
+    createBotCmdAdapter(event) {
+
+    }
+}
+
+module.exports = LineEventFactory;
