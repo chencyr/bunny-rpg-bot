c3875f5c4de059cd18c39b90e26acc15f400fffc
rexchen <chen.cyr@gmail.com>
2021-06-12 21:58:08 +0800

    add error handle

--- core/game/action/invite.js
+++ core/game/action/invite.js
@@ -62,24 +62,31 @@ class Attack
         this.isObjectCanAction(from);
         this.isActionCanToObject(to);
 
-        const type = 'character';
-        if (this.context.hasObject(type, from)) {
-            this.messages = {type: 'text', text: `ㄟㄟ!! 你還沒加入不能邀請別人啦!!`};
+        try {
+            const type = 'character';
+            if (this.context.hasObject(type, from)) {
+                this.messages = {type: 'text', text: `ㄟㄟ!! 你還沒加入不能邀請別人啦!!`};
+            }
+            const fromObj = this.context.getObject(type, from);
+
+            if (this.context.hasObject(type, to)) {
+                const toObj = this.context.getObject(type, to);
+                this.messages = {type: 'text', text: `${toObj.getName()}早就加入拉!! ${fromObj.getName()}你失憶了喔?!`};
+            }
+
+            const result = this.context.newCharacter({
+                userId: to.userId,
+                name: args[0]
+            });
+
+            const name = result.characterName;
+            this.messages = {type: 'text', text: `${name}, 恭喜您!! 成功被 [${fromObj.getName()}] 加入這個糞Game!!`};
+
         }
-        const fromObj = this.context.getObject(type, from);
-
-        if (this.context.hasObject(type, to)) {
-            const toObj = this.context.getObject(type, to);
-            this.messages = {type: 'text', text: `${toObj.getName()}早就加入拉!! ${fromObj.getName()}你失憶了喔?!`};
+        catch (e) {
+            console.info("GameEngine: join action:", e);
+            this.messages = {type: 'text', text: `邀請失敗!! 不要亂搞!! \n你看錯誤發生了\n${e}`};
         }
-
-        const result = this.context.newCharacter({
-            userId: to.userId,
-            name: args[0]
-        });
-
-        const name = result.characterName;
-        this.messages = {type: 'text', text: `${name}, 恭喜您!! 成功被 [${fromObj.getName()}] 加入這個糞Game!!`};
 
         return this;
     }
