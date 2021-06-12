8095965b1458e3748af1305aed50c32123b0adf2
rexchen <chen.cyr@gmail.com>
2021-06-12 14:45:58 +0800

    implement architecture

---
+++ core/events/event-factory/line-command-event-adapter.js
@@ -0,0 +1,11 @@
+const Adapter = require('../command/bot-command-event-adapter');
+
+/**
+ * LineCommandEventAdapter base class
+ */
+class LineCommandEventAdapter extends Adapter
+{
+
+}
+
+module.exports = LineCommandEventAdapter;
