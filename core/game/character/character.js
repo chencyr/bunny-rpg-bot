3095efbc9759ecc6c6315685d8c99207cbf8aa5b
rexchen <chen.cyr@gmail.com>
2021-06-12 21:08:03 +0800

    implement join action

--- core/game/character/character.js
+++ core/game/character/character.js
@@ -4,7 +4,31 @@
  */
 class Character
 {
+    /**
+     * Init player
+     * @param initInfo
+     * @constructor
+     */
+    constructor(initInfo) {
+        this.name = initInfo.name;
+        this.userId = initInfo.userId;
+        this.level = 1;
+        this.job = "無職業";
+        this.status = {
+            hp: 100,
+            mp: 20,
+            str: 10,
+            vit: 10,
+            dex: 10,
+            agi: 10,
+            int: 10,
+            luk: 10,
+        }
+    }
 
+    getStatus() {
+        return this.status;
+    }
 }
 
 module.exports = Character;
--- core/game/character/player/player.js
+++ core/game/character/player/player.js
@@ -6,6 +6,62 @@ const Character = require('../character');
  */
 class Player extends Character
 {
+    /**
+     * Init player
+     * @param initInfo
+     * @constructor
+     */
+    constructor(initInfo) {
+        super(initInfo);
+        this.job = "路人";
+
+        this.status = {
+            hp: this.computeHP(),
+            mp: this.computeMP(),
+            str: this.computeSTR(),
+            vit: this.computeVIT(),
+            dex: this.computeDEX(),
+            agi: this.computeAGI(),
+            int: this.computeINT(),
+            luk: this.computeLUK(),
+        }
+    }
+
+    getRandom(min,max){
+        return Math.floor(Math.random()*(max-min+1))+min;
+    };
+
+    computeHP() {
+        return this.getRandom(100, 500);
+    }
+
+    computeMP() {
+        return this.getRandom(20, 500);
+    }
+
+    computeSTR() {
+        return this.getRandom(10, 30);
+    }
+
+    computeVIT() {
+        return this.getRandom(10, 30);
+    }
+
+    computeDEX() {
+        return this.getRandom(10, 30);
+    }
+
+    computeAGI() {
+        return this.getRandom(10, 30);
+    }
+
+    computeINT() {
+        return this.getRandom(10, 30);
+    }
+
+    computeLUK() {
+        return this.getRandom(10, 30);
+    }
 
 }
 
