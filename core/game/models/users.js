277b5fc4e026451093305bb1d8ed1034e008d82d
rexchen <chen.cyr@gmail.com>
2021-06-15 02:05:27 +0800

    refactor relation method.

--- core/game/models/users.js
+++ core/game/models/users.js
@@ -1,9 +1,13 @@
 const Model = require('./model');
+const Characters = require('./characters');
 
+/**
+ * Characters users class
+ */
 class Users extends Model
 {
     characters() {
-        return this.oneToMany('characters', 'user_id');
+        return this.oneToMany(Characters, 'user_id');
     }
 }
 
