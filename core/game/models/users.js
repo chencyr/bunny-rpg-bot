3bda0499310119c7cc4b80ab60a4a9c073936e07
rexchen <chen.cyr@gmail.com>
2021-06-14 23:35:20 +0800

    remove unused code

--- core/game/models/users.js
+++ core/game/models/users.js
@@ -5,34 +5,6 @@ class Users extends Model
     characters() {
         return this.oneToMany('characters', 'user_id');
     }
-
-    get yo() {
-        return 'yo'
-    }
 }
-
-const model = new Users();
-
-async function getUserRecord(obj, id) {
-    obj = new Users();
-    const result = await obj
-        // .join('characters', 'characters.user_id', 'users.id')
-        .characters()
-        .select('users.id')
-        .where('characters.user_id', '=' , id);
-
-    console.log(result);
-    return result;
-}
-
-getUserRecord(model, 5);
-getUserRecord(model, 5);
-
-
-console.log('getTable', model.getTable());
-console.log('yo', model.yo);
-
-
-
 
 module.exports = Users;
