19ddbc444a3149501c2ae5bda4f36daed21d5bb0
rexchen <chen.cyr@gmail.com>
2021-06-16 22:00:10 +0800

    add protect for dead state

--- core/game/services/character-service/character/state/dead.js
+++ core/game/services/character-service/character/state/dead.js
@@ -12,6 +12,23 @@ class Dead extends State
     toString() {
         return "死亡";
     }
+
+    /**
+     * Create damage for attack other object.
+     * @return {{accuracy: number, value: number}}
+     */
+    createDamage() {
+        throw new Error("Cannot create damage in dead state.");
+    }
+
+    /**
+     * Received other object's damage.
+     * @param damage
+     * @return {{isDodge: boolean, isCritical: boolean, exp: number, damageHp: number}}
+     */
+    receiveDamage(damage) {
+        throw new Error("Cannot received damage in dead state.");
+    }
 }
 
 module.exports = Dead;
--- core/game/services/character-service/character/state/normal.js
+++ core/game/services/character-service/character/state/normal.js
@@ -12,6 +12,23 @@ class Normal extends State
     toString() {
         return "正常";
     }
+
+    /**
+     * Create damage for attack other object.
+     * @return {{accuracy: number, value: number}}
+     */
+    createDamage() {
+        return super.createDamage();
+    }
+
+    /**
+     * Received other object's damage.
+     * @param damage
+     * @return {{isDodge: boolean, isCritical: boolean, exp: number, damageHp: number}}
+     */
+    receiveDamage(damage) {
+        return super.receiveDamage(damage);
+    }
 }
 
 module.exports = Normal;
