a344440313221479de2d753743ea20efe086e436
rexchen <chen.cyr@gmail.com>
2021-06-16 14:07:37 +0800

    refactor attack by class Action.

--- core/game/actions/attack.js
+++ core/game/actions/attack.js
@@ -1,16 +1,16 @@
-
+const Action = require('./action');
 
 /**
  * Attack action.
  */
-class Attack
+class Attack extends Action
 {
     /**
      * Constructor
      * @param context
      */
     constructor(context) {
-        this.context = context;
+        super(context);
     }
 
     /**
@@ -34,101 +34,53 @@ class Attack
         ];
     }
 
-
     /**
-     * Check object can do the action.
-     * @param object
-     * @return {boolean}
-     */
-    isObjectCanAction(object) {
-        return true;
-    }
-
-    /**
-     * Check action to the object.
-     * @param object
-     */
-    isActionCanToObject(object) {
-        return true;
-    }
-
-    /**
-     * Execute action.
+     * Execute action for child class implement
      * @param from
      * @param to
      * @param args
-     * @return this
      */
-    async exec(from, to, args) {
-        this.isObjectCanAction(from);
-        this.isActionCanToObject(to);
+    async handler(from, to, args) {
+        const characterService = this.context.getService('character-service');
 
-        try {
-            const characterService = this.context.getService('character-service');
-
-            if (to.length == 0) {
-                if (! args[0]) {
-                    throw new Error('Cannot find character error');
-                }
-                to.characterId = args[0];  
-            }
-
-            const character1 = await characterService.getById(from.characterId);
-            const character2 = await characterService.getById(to.characterId);
-
-            if (!character1 || !character2) {
+        if (to.length == 0) {
+            if (! args[0]) {
                 throw new Error('Cannot find character error');
             }
-
-            const damage = character1.createDamage();
-            const result = character2.receiveDamage(damage);
-
-            this.messages = [];
-
-            if (result.isMiss == true) {
-                this.messages.push({
-                    type: 'text',
-                    text: `${character1.getName()} 攻擊了 ${character2.getName()}，但技巧很差被 ${character2.getName()} 閃過了!!`
-                });
-                return this;
-            }
-
-            this.messages.push({
-                type: 'text',
-                text: `${character1.getName()} 攻擊了 ${character2.getName()}，${character2.getName()} 受到 -${result.damageHp} HP 損傷!!`
-            });
-
-            if (result.hp == 0) {
-                this.messages.push({
-                    type: 'text',
-                    text: `${character2.getName()} 已死亡倒在地上抖動!!`
-                });
-            }
-
-            if (result.exp > 0) {
-                const isLevelUp = character1.receivedExp(result.exp);
-                let expText = `${character1.getName()} 獲得經驗值 ${result.exp} exp!!`;
-                if(isLevelUp) {
-                    expText += ` 你的等級提升了!! LV.${character1.getLevel()}`;
-                }
-                this.messages.push({
-                    type: 'text',
-                    text: expText
-                });
-            }
-        }
-        catch (e) {
-            console.error("GameEngine: action:", e);
-            this.messages = {type: 'text', text: `不要亂搞!! 你看噴錯誤了啦!!\n${e}`};
+            to.characterId = args[0];
         }
 
-        return this;
-    }
+        const character1 = await characterService.getById(from.characterId);
+        const character2 = await characterService.getById(to.characterId);
 
-    getMessages() {
-        return this.messages;
-    }
+        if (!character1 || !character2) {
+            throw new Error('Cannot find character error');
+        }
 
+        const damage = character1.createDamage();
+        const result = character2.receiveDamage(damage);
+
+        this.messages = [];
+
+        if (result.isMiss == true) {
+            this.writeMsg(`${character1.getName()} 攻擊了 ${character2.getName()}，但技巧很差被 ${character2.getName()} 閃過了!!`);
+            return this;
+        }
+
+        this.writeMsg(`${character1.getName()} 攻擊了 ${character2.getName()}，${character2.getName()} 受到 -${result.damageHp} HP 損傷!!`);
+        if (result.hp == 0) {
+            this.writeMsg(`${character2.getName()} 已死亡倒在地上抖動!!`);
+        }
+
+        if (result.exp > 0) {
+            const isLevelUp = character1.receivedExp(result.exp);
+            let expText = `${character1.getName()} 獲得經驗值 ${result.exp} exp!!`;
+            if(isLevelUp) {
+                expText += ` 你的等級提升了!! LV.${character1.getLevel()}`;
+            }
+            this.writeMsg(expText);
+        }
+    }
 }
 
 module.exports = Attack;
