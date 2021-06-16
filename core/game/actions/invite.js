85037f46ab6f74a2ac316aa47cdbe32dac99f48d
rexchen <chen.cyr@gmail.com>
2021-06-16 16:52:06 +0800

    refactor by class Action

--- core/game/actions/invite.js
+++ core/game/actions/invite.js
@@ -1,16 +1,16 @@
-
+const Action = require('./action');
 
 /**
- * Attack action.
+ * Invite action.
  */
-class Attack
+class Invite extends Action
 {
     /**
      * Constructor
      * @param context
      */
     constructor(context) {
-        this.context = context;
+        super(context);
     }
 
     /**
@@ -32,92 +32,34 @@ class Attack
         ];
     }
 
-
     /**
-     * Check object can do the action.
-     * @param object
-     * @return {boolean}
-     */
-    isObjectCanAction(object) {
-        return true;
-    }
-
-    /**
-     * Check action to the object.
-     * @param object
-     */
-    isActionCanToObject(object) {
-        return true;
-    }
-
-    /**
-     * Execute action.
+     * Execute action for child class implement
      * @param from
      * @param to
      * @param args
-     * @return this
      */
-    async exec(from, to, args) {
-        // TODO: Test after refactor
-        this.isObjectCanAction(from);
-        this.isActionCanToObject(to);
+    async handler(from, to, args) {
+        const userService = this.context.getService('user-service');
+        const characterService = this.context.getService('character-service');
 
-        try {
-            const userService = this.context.getService('user-service');
-            const characterService = this.context.getService('character-service');
+        let counter = 0;
+        let names = "";
 
-            let counter = 0;
-            let names = "";
-
-            for(let item in to) {
-                if (! await userService.has(item.userId)) {
-                    throw new Error('Invalid user error: user not found.');
-                }
-
-                const character = { userId: item.userId, name: args[counter] };
-                await characterService.new('player', character);
-                names += `${character.name}, `;
-
-                counter++;
+        for(let index in to) {
+            const item = to[index];
+            if (! await userService.has(item.userId)) {
+                throw new Error('Invalid user error: user not found.');
             }
 
-            this.messages = {type: 'text', text: `${names}恭喜您成功加入這個糞Game!!`};
+            const character = { userId: item.userId, name: args[counter] };
+            await characterService.new('player', character);
+            names += `${character.name}, `;
 
-            // const type = 'character';
-            // if (!this.context.hasObject(type, from)) {
-            //     this.messages = {type: 'text', text: `ㄟㄟ!! 你還沒加入不能邀請別人啦!!`};
-            //     return this;
-            // }
-            // const fromObj = this.context.getObject(type, from);
-            //
-            // if (this.context.hasObject(type, to)) {
-            //     const toObj = this.context.getObject(type, to);
-            //     this.messages = {type: 'text', text: `${toObj.getName()}早就加入拉!! ${fromObj.getName()}你失憶了喔?!`};
-            //     return this;
-            // }
-            //
-            // const result = this.context.newCharacter({
-            //     userId: to[0].userId,
-            //     name: args[0]
-            // });
-            //
-            // const name = result.characterName;
-            // this.messages = {type: 'text', text: `${name}, 恭喜您!! 成功被 [${fromObj.getName()}] 加入這個糞Game!!`};
-
-            return this;
-        }
-        catch (e) {
-            console.error("GameEngine: action:", e);
-            this.messages = {type: 'text', text: `不要亂搞!! 你看噴錯誤了啦!!\n${e}`};
+            counter++;
         }
 
-        return this;
+        this.writeMsg(`${names}恭喜您成功加入這個糞Game!!`);
     }
-
-    getMessages() {
-        return this.messages;
-    }
-
 }
 
-module.exports = Attack;
+module.exports = Invite;
