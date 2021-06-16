25b512ccd1fef6869d2f6c46d225a2d59829be14
rexchen <chen.cyr@gmail.com>
2021-06-16 19:43:54 +0800

    change msg

--- core/game/actions/ident.js
+++ core/game/actions/ident.js
@@ -62,7 +62,7 @@ class Ident extends Action
         const status = character2.getStatus();
 
         let text = `名稱: ${character2.getName()}   等級: ${character2.getLevel()}   職業: ${character2.getJob()}\n`;
-        text += `狀態:${character2.getState()}   稱號: ${character2.getTitle()}\n`;
+        text += `狀態: ${character2.getState()}   稱號: ${character2.getTitle()}\n`;
         text += `經驗值: ${character2.getExp()}\n`;
         text += `HP: ${status.hp}/${status.max_hp}    MP: ${status.mp}/${status.max_mp}\n`;
         text += `STR: ${status.str}    VIT: ${status.vit}\n`;
