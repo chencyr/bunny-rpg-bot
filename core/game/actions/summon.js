9b25d65f1695280dfe5f624084d67f0a36058e83
rexchen <chen.cyr@gmail.com>
2021-06-16 14:01:58 +0800

    change message

--- core/game/actions/summon.js
+++ core/game/actions/summon.js
@@ -49,9 +49,10 @@ class Summon extends Action
 
         const monster = await characterService.new('monster', {});
 
-        this.writeMsg(`${player.getName()}, 成功召喚了一隻極為兇猛的怪物 !!`)
+        this.writeMsg(`${player.getName()} 成功召喚了一隻極為兇猛的怪物 !!`)
+            .writeMsg(`其偉大的名字為...「${monster.getName()}」!!!`)
+            .writeMsg("")
             .writeMsg(`${monster.getStatus().id}`)
-            .writeMsg(`其偉大的名字為...${monster.getName()}」!!!`)
     }
 }
 
