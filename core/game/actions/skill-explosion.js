fbe2db55edc9681fcb3231d18ef639b72e205eeb
rexchen <chen.cyr@gmail.com>
2021-06-19 05:27:55 +0800

    refactor use interaction action module

--- core/game/actions/skill-explosion.js
+++ core/game/actions/skill-explosion.js
@@ -1,18 +1,10 @@
-const Action = require('./action');
+const Action = require('./interaction');
 
 /**
  * Skill action.
  */
 class SkillExplosion extends Action
 {
-    /**
-     * Constructor
-     * @param context
-     */
-    constructor(context) {
-        super(context);
-    }
-
     /**
      * Get action ID.
      * @return {string}
@@ -32,47 +24,6 @@ class SkillExplosion extends Action
         ];
     }
 
-    /* abstract */
-    /**
-     * Get send game object for this action.
-     * @param from
-     * @param args
-     * @return {Promise<void>}
-     */
-    async getSendObjects(from, args) {
-        const characterService = this.context.getService('character-service');
-        let ids = [from]
-            .reduce((carry, item) => carry.concat([item.characterId]), [])
-        return await characterService.getByIds(ids);
-    }
-
-    /* abstract */
-    /**
-     * Get received game object for this action.
-     * @param to
-     * @param args
-     * @return {Promise<Array>}
-     */
-    async getReceivedObjects(to, args) {
-        const characterService = this.context.getService('character-service');
-        let ids = to
-            .reduce((carry, item) => carry.concat([item.characterId]), [])
-            .filter((item) => item === undefined)
-            .concat(args);
-
-        ids = this.filterDuplicate(ids);
-        return await characterService.getByIds(ids);
-    }
-
-    /**
-     * Remove duplicate value in array.
-     * @param arr {array}
-     * @return {array}
-     */
-    filterDuplicate (arr) {
-        return arr.filter((item, index) => arr.indexOf(item) === index);
-    }
-
     beforeInteraction(senders, receivers) {
         if (senders.length == 0) {
             this.writeMsg(`沒有可執行 [${this.getNames()[0]}] 此操作的對象`); return false;
@@ -83,13 +34,6 @@ class SkillExplosion extends Action
         }
     }
 
-    afterInteraction(senders, receivers) {
-
-    }
-
-    beforeSend(sender, receivers, args) {
-
-    }
 
     sending(sender, receivers, args) {
         // TODO: refactor use magic attack (compute by int)
@@ -105,9 +49,6 @@ class SkillExplosion extends Action
         this.writeMsg('吾名惠惠。紅魔族首屈一指的魔法師，操縱爆裂魔法之人。好好見識吾之力量吧！Explosion !!').sendMsg();
     }
 
-    beforeReceived(interaction, sender, receiver, args) {
-
-    }
 
     receiving(interaction, sender, receiver, args) {
         const characterService = this.context.getService('character-service');
@@ -134,45 +75,6 @@ class SkillExplosion extends Action
         return result;
     }
 
-    afterReceived(result, interaction, sender, receiver, args) {
-
-    }
-
-    /**
-     * Execute action for child class implement
-     * @param from
-     * @param to
-     * @param args
-     */
-    async handler(from, to, args) {
-        const receivers = await this.getReceivedObjects(to, args);
-        const senders = await this.getSendObjects(from, args);
-
-        if (this.beforeInteraction(senders, receivers, args)) {
-            return this;
-        }
-
-        for (let sender of senders.values()) {
-
-            this.beforeSend(sender, receivers, args);
-            const interaction = this.sending(sender, receivers, args);
-            this.afterSend(interaction, sender, receivers, args);
-
-            for(let receiver of receivers.values()) {
-
-                this.beforeReceived(interaction, sender, receiver, args);
-                const result = this.receiving(interaction, sender, receiver, args);
-                if (result.break) break;
-                if (result.continue) continue;
-                if (result.terminate) return this;
-                this.afterReceived(result, interaction, sender, receiver, args);
-            }
-        }
-
-        if (this.afterInteraction(senders, receivers, args)) {
-            return this;
-        }
-    }
 }
 
 module.exports = SkillExplosion;
