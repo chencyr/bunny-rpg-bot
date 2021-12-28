1c9df2897310bca0011d31bd68cacc75af3456c4
rexchen <chen.cyr@gmail.com>
2021-12-29 00:28:56 +0800

    refactor

--- discord.js
+++ discord.js
@@ -1,70 +1,116 @@
 const { Client, Intents } = require('discord.js');
 
-module.exports = function (app, EventFactory) {
 
-    const config = app.config('discord');
-    const factory = new EventFactory();
-    const options = { gameEngine: app.gameEngine, app: app };
-
-    const client = new Client({
+/**
+ * Create new discord client instance.
+ *
+ * @return {module.exports.Client|Client<boolean>}
+ */
+function getClient() {
+    return new Client({
         intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
     });
+}
 
-    async function handleMessage(message) {
-        const cmdEvent = factory.createBotCmdAdapter(message, options);
-        return await cmdEvent.trigger();
-    }
-
-    function isCommandText(text) {
+/**
+ * Create validator for check first word.
+ *
+ * @param keyword
+ * @return {Function}
+ */
+function firstWordIs(keyword) {
+    return (text) => {
         try {
-            return (text.substring(0, 1) == ':');
+            return (text.substring(0, 1) == keyword);
         }
         catch (e) {
             return false;
         }
     }
+}
+
+/**
+ * Create game engine adapter.
+ *
+ * @param factory
+ * @param app
+ * @return {function(*=)}
+ */
+function gameCommandAdapter(factory, app) {
+    return async (message) => {
+        const options = { gameEngine: app.gameEngine, app: app };
+        const cmdEvent = factory.createBotCmdAdapter(message, options);
+        return await cmdEvent.trigger();
+    }
+}
+
+/**
+ * Reply message
+ *
+ * @param message
+ * @return {Function}
+ */
+function replyMessage(message) {
+    return (result) => {
+        if (result.notReply()) {
+            return null;
+        }
+
+        const resultMessages = result.getMessages();
+        if(!resultMessages) {
+            return null;
+        }
+
+        resultMessages.forEach((result) => {
+            if(result.type == 'text') {
+                message.reply(result.text);
+            }
+        });
+    }
+}
+
+/**
+ * Dispatch message data by game-engine command
+ *
+ * @param validator Function creator
+ * @param dispatcher Function creator
+ * @param replyMessage Function creator
+ * @return {Function}
+ */
+function dispatchMessage(validator, dispatcher, replyMessage) {
+    return (message) => {
+        if(!validator(message.content)) {
+            return;
+        }
+
+        Promise
+            .resolve(dispatcher(message))
+            .then(replyMessage(message));
+    };
+}
+
+/**
+ * Bootstrap discord webhook.
+ *
+ * @param app {gameEngine}
+ * @param EventFactory
+ */
+function bootstrap(app, EventFactory) {
+    const config = app.config('discord');
+    const factory = new EventFactory();
+    const client = getClient();
 
     client.on("ready", () => {
         console.log("Discord Webhook was ready.");
     });
 
-    client.on("messageCreate", (message) => {
-        // console.log(message);
-
-        if (message.content.startsWith("/凱文")) {
-            message.channel.send("凱文又在亂把妹紙了!");
-        }
-
-        if (message.content.startsWith("/吠吠")) {
-            message.channel.send("回歸大自然～");
-        }
-
-        if (message.content.startsWith("HI Bunny")) {
-            message.channel.send("HI~HI~");
-        }
-
-        if (message.content.startsWith("跟助教一樣渣...")) {
-            message.reply("但奶捲好像會用Vite打包妹ㄗ...到底誰比較渣...");
-        }
-
-        if(isCommandText(message.content)) {
-            Promise.resolve(handleMessage(message)).then((result) => {
-                if (result.notReply()) {
-                    return null;
-                }
-
-                const messages = result.getMessages();
-                if(messages) {
-                    messages.forEach((msg) => {
-                        if(msg.type == 'text') {
-                            message.reply(msg.text);
-                        }
-                    });
-                }
-            });
-        }
-
-    });
+    client.on("messageCreate", dispatchMessage(
+        firstWordIs(":"),
+        gameCommandAdapter(factory, app),
+        replyMessage
+    ));
 
     client.login(config.botToken);
-};
+}
+
+module.exports = bootstrap;
