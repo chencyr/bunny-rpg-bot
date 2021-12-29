4cc4cbb129fc4f92a7ec39babf9e1b5a34ee6167
rexchen <chen.cyr@gmail.com>
2021-12-29 18:17:00 +0800

    add example file.

---
+++ configs/discord.example.js
@@ -0,0 +1,10 @@
+
+/**
+ * Configuration for Discord applications
+ *
+ * @type {{applicationID: string, botToken: string}}
+ */
+module.exports = {
+    botToken: 'BOT_TOKEN',
+    applicationID: 'APP_ID',
+};
