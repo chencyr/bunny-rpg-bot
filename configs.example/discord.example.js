baa17071bca8954bafffb50aa6692ef39ce86941
rexchen <chen.cyr@gmail.com>
2021-12-29 18:36:18 +0800

    update

---
+++ configs.example/discord.example.js
@@ -0,0 +1,10 @@
+
+/**
+ * Configuration for Discord applications
+ *
+ * @type {{applicationID: string, botToken: string}}
+ */
+module.exports = {
+    botToken: 'BOT_TOKEN_',
+    applicationID: 'APPLICATION_ID_',
+};
---
+++ configs.example/line.example.js
@@ -0,0 +1,11 @@
+/**
+ * Configuration for LINE applications.
+ *
+ * @type {{clientID: string, channelAccessToken: string, channelSecret: string}}
+ */
+module.exports = {
+    channelAccessToken: 'CHANNEL_ACCESS_TOKEN_',
+    channelSecret: 'CHANNEL_SECRET_',
+    clientID: 'CLIENT_ID_',
+
+};
