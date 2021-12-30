ce1b4bf6f0816603cba32971d4be09282ce495e5
rexchen <chen.cyr@gmail.com>
2021-12-31 00:12:20 +0800

    refactor

--- core/game/actions/status.js
+++ core/game/actions/status.js
@@ -46,15 +46,16 @@ class Status extends Action
             this.writeMsg(`抓到了吼～你還沒加入！\n輸入 /join {角色名稱} 加入這個美好相殘的故事八`);
             return this;
         }
-        const status = player.getStatus();
 
         let text = `名稱: ${player.getName()}   等級: ${player.getLevel()}   職業: ${player.getJob()}\n`;
         text += `狀態:${player.getState()}   稱號: ${player.getTitle()}\n`;
         text += `經驗值: ${player.getExp()}/${player.getNextExp()}\n`;
-        text += `HP: ${status.hp}/${status.max_hp}    MP: ${status.mp}/${status.max_mp} SP: ${status.sp}/${status.max_sp}\n`;
-        text += `STR: ${status.str}    VIT: ${status.vit}\n`;
-        text += `DEX: ${status.dex}    AGI: ${status.agi}\n`;
-        text += `INT: ${status.int}    LUK: ${status.luk}\n`;
+        text += `HP: ${player.currentHP}/${player.maxHP}\n`;
+        text += `MP: ${player.currentMP}/${player.maxMP}\n`;
+        text += `SP: ${player.currentSP}/${player.maxSP}\n`;
+        text += `STR: ${player.str}    VIT: ${player.vit}\n`;
+        text += `DEX: ${player.dex}    AGI: ${player.agi}\n`;
+        text += `INT: ${player.int}    LUK: ${player.luk}\n`;
 
         this.writeMsg(text);
     }
