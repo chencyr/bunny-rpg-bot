41d94918e21d14925594de25ac4a97f297d85d93
rexchen <chen.cyr@gmail.com>
2022-01-01 01:50:57 +0800

    adjust layout

--- core/game/actions/utils/output-status-board.js
+++ core/game/actions/utils/output-status-board.js
@@ -13,22 +13,23 @@ const signFormatter = function (number) {
  * @return {string}
  */
 const OutputStatusBoard = function(character) {
-    let text = `名稱: ${character.getName()}   等級: ${character.getLevel()}   職業: ${character.getJob()}\n`;
+    let text = `名稱 : ${character.getName()}   等級 : ${character.getLevel()}\n`;
     text += '-----------------------------------------------\n';
-    text += `狀態: ${character.getState()}   稱號: ${character.getTitle()}\n`;
+    text += `狀態 : ${character.getState()}  \n`;
     text += '-----------------------------------------------\n';
-    text += `經驗值: ${character.getExp()}/${character.getNextExp()}\n`;
+    text += `累計EXP : ${character.getExp()}/${character.getNextExp()}\n`;
+    text += `擊殺EXP : ${character.toExp()}\n`;
     text += '-----------------------------------------------\n';
-    text += `HP: ${character.currentHP}/${character.maxHP}\n`;
-    text += `MP: ${character.currentMP}/${character.maxMP}\n`;
-    text += `SP: ${character.currentSP}/${character.maxSP}\n`;
+    text += `HP : ${character.currentHP}/${character.maxHP}\n`;
+    text += `MP : ${character.currentMP}/${character.maxMP}\n`;
+    text += `SP : ${character.currentSP}/${character.maxSP}\n`;
     text += '-----------------------------------------------\n';
-    text += `STR: ${character.str} (${signFormatter(character.adjustSTR)})    VIT: ${character.vit} (${signFormatter(character.adjustVIT)})\n`;
-    text += `DEX: ${character.dex} (${signFormatter(character.adjustDEX)})    AGI: ${character.agi} (${signFormatter(character.adjustAGI)})\n`;
-    text += `INT: ${character.int} (${signFormatter(character.adjustINT)})    LUK: ${character.luk} (${signFormatter(character.adjustLUK)})\n`;
+    text += `STR : ${character.str} (${signFormatter(character.adjustSTR)})    VIT : ${character.vit} (${signFormatter(character.adjustVIT)})\n`;
+    text += `DEX : ${character.dex} (${signFormatter(character.adjustDEX)})    AGI : ${character.agi} (${signFormatter(character.adjustAGI)})\n`;
+    text += `INT : ${character.int} (${signFormatter(character.adjustINT)})    LUK : ${character.luk} (${signFormatter(character.adjustLUK)})\n`;
     text += '-----------------------------------------------\n';
 
-    text += `加護: \n`;
+    text += `加護 & 詛咒: \n`;
     character.getBuffs().forEach((buff) => {
         text += `${buff.getDisplayName()} \n`;
     });
