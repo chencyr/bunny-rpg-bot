1b03003de2a03332b012e5fee23e6aaab6eea64e
rexchen <chen.cyr@gmail.com>
2022-01-02 00:21:08 +0800

    add new skill, buff

---
+++ core/game/services/character-service/character/buff/full-gas-95-buff.js
@@ -0,0 +1,156 @@
+const StandardBuff = require('./standard-buff');
+
+/**
+ * FullGas95
+ */
+class FullGas95 extends StandardBuff
+{
+    /**
+     * Constructor
+     *
+     * @param character
+     */
+    constructor(character) {
+        super(character);
+    }
+
+    /**
+     * Internal inject for ignore property.
+     * @return {function(*, *, *, *): boolean}
+     */
+    getIgnoreCondition() {
+        return (repeatIndex, sender, receiver, args) => receiver.isState('dead');
+    }
+
+    /**
+     * Get buff ID.
+     * @return {string}
+     */
+    getId() {
+        return "full-gas-95";
+    }
+
+    /**
+     * Get buff alias names
+     * @return {Array}
+     */
+    getNames() {
+        return [
+            "full-gas-95",
+        ];
+    }
+
+    /**
+     * Get buff name
+     *
+     * @return {string}
+     */
+    getDisplayName() {
+        return `95加滿 (${(this.getEffectTime()/1000) - this.timer}s)`
+    }
+
+    /**
+     * Buff up
+     * @param options
+     */
+    up(options) {
+
+    }
+
+    /**
+     * Buff down
+     * @param options
+     */
+    down(options) {
+
+    }
+
+    /**
+     * Effective
+     */
+    effect() {
+        const character = this.getContext();
+        const max = character.maxHP;
+        const regen = Math.floor(max * 0.1);
+
+        if(max == character.currentHP) {
+            return;
+        }
+
+        if(character.currentHP + regen > max) {
+            character.currentHP = max;
+        }
+        else {
+            character.currentHP += regen;
+        }
+        console.log(`[${this.getDisplayName()}] Character ${character.getName()} regen Hp +${regen}, Current HP: ${character.currentHP}/${max}`);
+    }
+
+    /**
+     * Define buff frequency
+     *
+     * @return {number} ms
+     */
+    getFrequency() {
+        return 1000 * 1;
+    }
+
+    /**
+     * Define buff effect time
+     *
+     * @return {number} ms, if time = 0 forever
+     */
+    getEffectTime() {
+        return 1000 * 60 * 5;
+    }
+
+    /**
+     * Getter for str
+     * @return {number}
+     */
+    get str() {
+        return -(this.context.statusProperty('str') * 2);
+    }
+
+    /**
+     * Getter for vit
+     * @return {number}
+     */
+    get vit() {
+        return (this.context.statusProperty('vit') * 2);
+    }
+
+    /**
+     * Getter for str
+     * @return {number}
+     */
+    get dex() {
+        return 0;
+    }
+
+    /**
+     * Getter for agi
+     * @return {number}
+     */
+    get agi() {
+        return 0;
+    }
+
+    /**
+     * Getter for int
+     * @return {number}
+     */
+    get int() {
+        return 0;
+    }
+
+    /**
+     * Getter for int
+     * @return {number}
+     */
+    get luk() {
+        return 0;
+    }
+}
+
+module.exports = FullGas95;
---
+++ core/game/services/skill-service/skill/full-gas-95.js
@@ -0,0 +1,104 @@
+const Skill = require('./skill');
+
+
+// TODO refactor by service getter
+const randomFromArray = require('../../../helpers/randomFromArray');
+const Buff = require('../../character-service/character/buff/full-gas-95-buff');
+
+/**
+ * FullGas95 skill
+ */
+class FullGas95 extends Skill
+{
+    /**
+     * Get display name.
+     * @return {string}
+     */
+    getDisplayName() {
+        return "95加滿";
+    }
+
+    /**
+     * Get standard name.
+     * @return {string}
+     */
+    getStandardName() {
+        return "full-gas-95";
+    }
+
+    /**
+     * Hooker for before interaction
+     * @param senders
+     * @param receivers
+     * @param action
+     * @param args
+     * @return {Promise<boolean>}
+     */
+    async beforeInteraction(senders, receivers, action, args) {
+        const data = {
+            skill: this,
+            senders: senders,
+            receivers: receivers,
+            action: action,
+            args: args
+        };
+
+        return await this.restrict(data,
+            'characters-has-skill',
+            'characters-cost-enough',
+            'receivers-is-character',
+        );
+    }
+
+    /**
+     * Get cost info
+     * @param options {object}
+     * @return {{mp: number, hp: number, sp: number}}
+     */
+    getCost(options) {
+        return {
+            hp: 0,
+            mp: 8000,
+            sp: 0,
+        };
+    }
+
+    /**
+     * Hooker for after interaction
+     * @param senders
+     * @param receivers
+     * @param action
+     * @param args
+     * @return {Promise<void>}
+     */
+    async afterInteraction(senders, receivers, action, args) {
+
+    }
+
+    /**
+     * Send effect data.
+     * @param sender
+     * @param receivers
+     * @param action
+     * @param args
+     * @return {Promise<void>}
+     */
+    async sending(sender, receivers, action, args) {
+
+        const buff = new Buff();
+
+        const slogan = randomFromArray([
+            "你要 +5，還是 +95？ >_0",
+        ]);
+
+        action.writeMsg(slogan).sendMsg();
+        // some adjust for buff.
+
+        this.cost(sender);
+
+        return buff;
+    }
+}
+
+
+module.exports = FullGas95;
