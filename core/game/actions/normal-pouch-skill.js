8b6a63adfeb413e4da9c450b4d7138134d2f00e1
rexchen <chen.cyr@gmail.com>
2022-01-02 01:40:54 +0800

    add action skill

---
+++ core/game/actions/normal-pouch-skill.js
@@ -0,0 +1,57 @@
+const Skill = require('./skill');
+
+const SendingBehavior = require('./skill/attack-sending-behavior');
+const ReceivingBehavior = require('./skill/attack-receiving-behavior');
+
+/**
+ * Skill action.
+ */
+class NormalPouchSkill extends Skill
+{
+    /**
+     * Get action ID.
+     * @return {string}
+     */
+    getId() {
+        return "normal-pouch-skill";
+    }
+
+    /**
+     * Get action master name.
+     * @return {string}
+     */
+    getNames() {
+        return [
+            "normal-pouch",
+            "普通一拳",
+            "琦玉一拳",
+            "一拳",
+        ];
+    }
+
+    /**
+     * Get skill name. (should same as skill class file name)
+     * @return {string} standard name
+     */
+    getSkillName() {
+        return "normal-pouch"
+    }
+
+    /**
+     * Get sending behavior.
+     * @return {SendingBehavior}
+     */
+    getSendingBehavior() {
+        return SendingBehavior;
+    }
+
+    /**
+     * Get receiving behavior.
+     * @return {ReceivingBehavior}
+     */
+    getReceivingBehavior() {
+        return ReceivingBehavior
+    }
+}
+
+module.exports = NormalPouchSkill;
